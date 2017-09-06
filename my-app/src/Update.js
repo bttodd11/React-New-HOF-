a4baac35a25b6e994b436ee97c753cd6fdc843ec
Brandon Pugh <bp@brandonpugh.com>
2017-09-06 13:29:01 -0500

    Quick fix to get form working
    Added an onchange and onsubmit handler to the form so the values can be
    added to state when submitting the form.
    Loops through the list of submissions to render a list.

--- my-app/src/Update.js
+++ my-app/src/Update.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
 import './Update.css';
 
+const getDefaultFormValues = () => ({
+  firstName: '',
+  lastName: '',
+  city: '',
+  state: '',
+});
+
 class Update extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      form: getDefaultFormValues(),
+      submissions: [],
+    };
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleFormChange(event) {
+    const form = this.state.form;
+    form[event.target.name] = event.target.value;
+    this.setState({form});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const submissions = this.state.submissions;
+    submissions.push(this.state.form);
+    this.setState({submissions, form: getDefaultFormValues()});
+  }
+
   render() {
+    const form = this.state.form;
     return (
       <div className="App">
-        <div className="App-header">
-        </div>
-      <div className="inputs">
-        <h1>Gary Sheffield Should Be In The Hall of Fame Petition</h1>
-        First Name: <input type="text" value="" />
-        <br></br>
-        Last Name: <input type="text" value="" />
-        <br></br>
-        City: <input type="text" value="" />
-        <br></br>
-        State: <input type="text" value="" />
-        <br></br>
-        <input type="submit" value="Submit" />
-      </div>
+        <div className="App-header" />
+        <form onChange={this.handleFormChange} onSubmit={this.handleSubmit}>
+          <div className="inputs">
+            <h1>Gary Sheffield Should Be In The Hall of Fame Petition</h1>
+            First Name: <input type="text" name="firstName" value={form.firstName} />
+            <br />
+            Last Name: <input type="text" name="lastName" value={form.lastName} /> 
+            <br />
+            City: <input type="text" name="city" value={form.city} />
+            <br />
+            State: <input type="text" name="state" value={form.state} /> 
+            <br />
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+        <ul>
+          {this.state.submissions.map((x, index) => (
+            <li key={index}>
+              {x.firstName} - {x.lastName}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
