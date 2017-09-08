9e7633314c9ed92c65998bdc42717b7d81cb3683
bttodd11 <bttodd@icloud.com>
2017-09-07 22:39:20 -0500

    Updated React

--- my-app/src/Update.js
+++ my-app/src/Update.js
@@ -1,42 +1,68 @@
 import React, { Component } from 'react';
 import './Update.css';
 
+const getDefaultFormValues = () => ({
+  firstName: '',
+  lastName: '',
+  city: '',
+  state: '',
+});
+
+
 class Update extends Component {
 constructor(props){
 super(props);
 this.state = {
-  names: props.names};
+  form: getDefaultFormValues(),
+  submissions:[],
+};
 
-  this.handleChange = this.handleChange.bind(this);
+  this.handleFormChange = this.handleFormChange.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
 }
-handleChange(event) {
-  this.setState({value: event.target.value});
+
+handleFormChange(event) {
+  const form = this.state.form;
+  form[event.target.name] = event.target.value;
+  this.setState({form});
 }
 
 handleSubmit(event){
-  alert('' + this.state.value);
   event.preventDefault();
+  const submissions = this.state.submissions;
+  submissions.push(this.state.form);
+  this.setState({submissions, form: getDefaultFormValues()});
 }
 
 
   render() {
+    const form = this.state.form;
     return (
+    <div>
+      <div className="App-header" />
+      <form onChange ={this.handleFormChange} onSubmit={this.handleSubmit}>
       <div className="inputs">
         <h1>Gary Sheffield Should Be In The Hall of Fame Petition</h1>
-        <form onSubmit ={this.handleSubmit}>
-        First Name: <input type="text" placeholder="First Name" value={this.state.value} onChange={this.handleChange}  />
+        First Name: <input type="text" placeholder="First Name" value={form.firstName}  />
         <br></br>
-        Last Name: <input type="text" placeholder="Last Name" />
+        Last Name: <input type="text" placeholder="Last Name" value={form.lastName} />
         <br></br>
-        City: <input type="text" placeholder="City" ref="city" />
+        City: <input type="text" placeholder="City" ref="city" value={form.city} />
         <br></br>
-        State: <input type="text" placeholder="State" ref="state" />
+        State: <input type="text" placeholder="State" ref="state" value={form.state} />
         <br></br>
-
-        <button type="submit" value="Submit"> Submit </button>
-      </form>
+        <input type="submit" value="Submit" />
       </div>
+      </form>
+      <ul>
+{this.state.submissions.map((x,index) => (
+  <li key={index}>
+    {x.firstName} - {x.lastName} - {x.city} - {x.state}
+  </li>
+))}
+      </ul>
+    </div>
+
     );
   }
 }
